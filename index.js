6bd60dcb2ea36e1b700096a1a08a62ba500f818d
samelie <sam@stinkdigital.com>
2018-05-15 20:56:12 -0700

    Remove old code

--- index.js
+++ index.js
@@ -1,69 +1,50 @@
-// const server = require("server")
-/*const { get, post, socket } = server.router
-const { render } = server.reply*/
+const server = require("server");
+const { get, post, socket } = server.router;
+const { render } = server.reply;
 
-const GL = require("./src/gl")
-const IM = require("./src/imagemagick")
-const Camera = require("./src/camera")
-const Output = require("./src/output")
-const {WEBM, MP4} = require("./src/outputs")
+const GL = require("./src/gl");
+const IM = require("./src/imagemagick");
+const Camera = require("./src/camera");
+const Output = require("./src/output");
+const { WEBM, MP4 } = require("./src/outputs");
 
-/*server(
+server(
   [
     get("/", ctx => {
-      ctx.session.counter = ctx.session.counter || 0
-      return render("index.html")
+      ctx.session.counter = ctx.session.counter || 0;
+      return render("index.html");
     }),
     get("/connect", ctx => {
-      ctx.session.counter = ctx.session.counter || 0
-      return render("index.html")
+      ctx.session.counter = ctx.session.counter || 0;
+      return render("index.html");
     }),
-    get("/start", ctx => {
-    }),
+    get("/start", ctx => {}),
   ],
+
   socket("connect", ctx => {
     // Emit an event every second with +1 on the session
     setInterval(() => {
       // Increment the counter
-      ctx.session.counter++
+      ctx.session.counter++;
 
       // For socket.io you need to manually save it
-      ctx.session.save()
+      ctx.session.save();
 
       // Send the value to the currently connected socket
-      ctx.socket.emit("message", ctx.session.counter)
-    }, 1000)
+      ctx.socket.emit("message", ctx.session.counter);
+    }, 1000);
   })
-)
-*/
+);
+
 /***************
  *
  ************** */
 
-const USE_OMX = false
-const USE_FFPLAY = true
-const USE_UDP = "udp://192.168.1.134:3333"
-const OMX = `- | mplayer -fps 30 -aspect 16:9 -fs -zoom -cache 1024 -`
-//const OMX =
- // "- | omxplayer -b -r -g --no-keys -s -I -z --timeout 60 -o hdmi --fps 1 pipe:0"
-const FFPLAY = "- | ffplay -"
-const ffoutput = USE_OMX ? OMX : USE_FFPLAY ? FFPLAY : USE_UDP;
-
 const CONFIG = {
- // width: 480,
-  //height: 360,
-width: 240,
+  width: 240,
   height: 180,
   fps: 3,
-}
-const IMG_COMMAND = [
-  "-depth",
-  "8",
-  "-size",
-  `${CONFIG.width}x${CONFIG.height}`,
-  "rgba:-",
-  "JPEG:-",
-];
+};
 
 let _converting = false;
 const gl = GL(CONFIG);
@@ -81,38 +62,28 @@ const output = Output({
     CONFIG.fps,
   ],
   output: [
- //   ...WEBM,
-//...MP4,
-    /*"-s",
-    `${CONFIG.width}x${CONFIG.height}`,
-    "-r",
-    CONFIG.fps,
-    "-framerate",
-    CONFIG.fps,*/
-   "-probesize","32", "-analyzeduration", "128","-use_wallclock_as_timestamps","1","-tune",
+    "-probesize",
+    "32",
+    "-analyzeduration",
+    "128",
+    "-use_wallclock_as_timestamps",
+    "1",
+    "-tune",
     "zerolatency",
-   // "-c:v",
-    //"mpeg1video",
     "-r",
     "30",
-    /*"-f",
-    "alsa",*/
-     "-an",
-//"udp://192.168.1.81:3333"
-    `"http://localhost:8090/feed2.ffm"`,
-"2>",
-"log.txt"
+    "-an",
+    `"http://localhost:8090/mjpeg.ffm"`,
+    "2>",
+    "log.txt",
   ],
 });
 const camera = Camera(gl, {
   fps: CONFIG.fps,
- ...CONFIG,
+  ...CONFIG,
   onFrame: buffer => {
     _converting = true;
     output.frame(buffer);
-    /*IM.convert(buffer, IMG_COMMAND, imageBuffer => {
-        _converting = false
-      })*/
   },
-})
-camera.play("http://192.168.1.160:8080/video.jpeg")
+});
+camera.play("http://192.168.1.160:8080/video.jpeg");
--- src/ffserver.js
+++ src/ffserver.js
@@ -1,12 +1,11 @@
 var spawn = require("child_process").spawn;
 var exec = require("child_process").exec;
 
-const ARGS = ["-c:v", "libvpx"];
 
 class FFServer {
   start() {
     this.ffserver = spawn("ffserver", ["-f", "../ffserver.conf"]);
-    return ARGS;
+    return this.ffserver
   }
 
   stop() {
