3b6b8c89ac51916e4647452dd05d223bd710f312
samelie <sam@stinkdigital.com>
2018-05-21 22:07:43 -0700

    disable ffserver

--- src/ffserver.js
+++ src/ffserver.js
@@ -3,11 +3,11 @@ const spawn = require("child_process").spawn;
 class Commmands {
   start() {
     spawn("export", ["DISPLAY=:0"]);
-    spawn("systemctl", ["start", "ffserver.service"]);
+    // spawn("systemctl", ["start", "ffserver.service"]);
   }
 
   stop() {
-    spawn("systemctl", ["stop", "ffserver.service"]);
+    // spawn("systemctl", ["stop", "ffserver.service"]);
   }
 }
 
